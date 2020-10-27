54d21350d999ea26265e6700753fa2d27d57c03f
tortoise10h <tortoise10h@gmail.com>
2020-10-27 23:22:33 +0700

    [Feature] get all purchase proposal v1 (acceptable)

---
+++ api/CQRS/PurchaseProposalForms/Queries/GetAllPurchaseProposalFormQuery.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using api.Contracts.V1.RequestModels;
+using api.Contracts.V1.ResponseModels;
+using api.Contracts.V1.ResponseModels.PurchaseProposalForms;
+using api.Helpers;
+using LanguageExt.Common;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using E = api.Entities;
+
+namespace src.CQRS.PurchaseProposalForms.Queries
+{
+    public class GetAllPurchaseProposalFormQuery : PaginationQuery, IRequest<Result<PagedResponse<PurchaseProposalFormResponse>>>
+    {
+
+    }
+
+    public class GetAllPurchaseProposalFormHandler : IRequestHandler<GetAllPurchaseProposalFormQuery, Result<PagedResponse<PurchaseProposalFormResponse>>>
+    {
+        private readonly DataContext _context;
+        private readonly IPaginationHelpers _paginationHelper;
+
+        public GetAllPurchaseProposalFormHandler(DataContext context, IPaginationHelpers paginationHelper)
+        {
+            _context = context;
+            _paginationHelper = paginationHelper;
+        }
+
+        public async Task<Result<PagedResponse<PurchaseProposalFormResponse>>> Handle(GetAllPurchaseProposalFormQuery query, CancellationToken cancellationToken)
+        {
+            var queryable = _context.PurchaseProposalForms.AsQueryable();
+            /** TODO: Include import bill */
+            var result = await _paginationHelper.Paginate<E.PurchaseProposalForm, PurchaseProposalFormResponse>(
+                queryable, query
+            );
+
+            return result;
+        }
+    }
+}
