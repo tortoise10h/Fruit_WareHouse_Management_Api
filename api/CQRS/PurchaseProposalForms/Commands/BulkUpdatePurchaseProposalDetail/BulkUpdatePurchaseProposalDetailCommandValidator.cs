575e67e5e9aee731035778381121b577744c4ba1
tortoise10h <tortoise10h@gmail.com>
2020-10-29 13:09:25 +0700

    [Feature] bulk update purchase proposal details

---
+++ api/CQRS/PurchaseProposalForms/Commands/BulkUpdatePurchaseProposalDetail/BulkUpdatePurchaseProposalDetailCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using api.CQRS.PurchaseProposalForms.Commands.CreatePurchaseProposalForms;
+using api.CQRS.PurchaseProposalForms.Commands.UpdatePurchaseProposalDetails;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.CQRS.PurchaseProposalForms.Commands.BulkUpdatePurchaseProposalDetails
+{
+    public class BulkCreatePurchaseProposalDetailCommandValidator: AbstractValidator<BulkUpdatePurchaseProposalDetailCommand>
+    {
+        public BulkCreatePurchaseProposalDetailCommandValidator()
+        {
+            RuleFor(x => x.PurchaseProposalDetails)
+                .NotNull()
+                    .WithMessage("Danh sách phải có ít nhất một sản phẩm")
+                .Must(x => x != null && x.Count > 0)
+                    .WithMessage("Danh sách phải có ít nhất một sản phẩm");
+
+            RuleForEach(x => x.PurchaseProposalDetails)
+                .SetValidator(new UpdatePurchaseProposalDetailCommandValidator());
+        }
+    }
+}
---
+++ api/CQRS/PurchaseProposalForms/Commands/UpdatePurchaseProposalDetails/UpdatePurchaseProposalDetailCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using api.Common.Enums;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.CQRS.PurchaseProposalForms.Commands.UpdatePurchaseProposalDetails
+{
+    public class UpdatePurchaseProposalDetailCommandValidator: AbstractValidator<UpdatePurchaseProposalDetailCommand>
+    {
+        public UpdatePurchaseProposalDetailCommandValidator()
+        {
+            RuleFor(x => x.Quantity)
+                .GreaterThan(0)
+                    .WithMessage("Số lượg không được phép nhỏ hơn 0");
+        }
+    }
+}
