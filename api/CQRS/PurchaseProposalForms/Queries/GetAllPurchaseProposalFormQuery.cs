c73ec27df68005928ba1c6c828efd784af4e9677
tortoise10h <tortoise10h@gmail.com>
2020-10-30 14:40:29 +0700

    [Feature] include user in list purchase proposal form

--- api/CQRS/PurchaseProposalForms/Queries/GetAllPurchaseProposalFormQuery.cs
+++ api/CQRS/PurchaseProposalForms/Queries/GetAllPurchaseProposalFormQuery.cs
@@ -31,8 +31,27 @@ namespace src.CQRS.PurchaseProposalForms.Queries
         public async Task<Result<PagedResponse<PurchaseProposalFormResponse>>> Handle(GetAllPurchaseProposalFormQuery query, CancellationToken cancellationToken)
         {
             var queryable = _context.PurchaseProposalForms.AsQueryable();
-            queryable = queryable.AsNoTracking();
             /** TODO: Include import bill */
+            queryable = queryable.AsNoTracking();
+            queryable = queryable
+                .Join(_context.Users,
+                    ppf => ppf.CreatedBy,
+                    u => u.Id,
+                    (ppf, u) =>
+                    new E.PurchaseProposalForm
+                    {
+                        Id = ppf.Id,
+                        Deadline = ppf.Deadline,
+                        Status = ppf.Status,
+                        OnTimeOrNotStatus = ppf.OnTimeOrNotStatus,
+                        Description = ppf.Description,
+                        ExceptionReason = ppf.ExceptionReason,
+                        CreatedAt = ppf.CreatedAt,
+                        LastModifiedAt = ppf.LastModifiedAt,
+                        User = u
+                    }
+                );
+
             var result = await _paginationHelper.Paginate<E.PurchaseProposalForm, PurchaseProposalFormResponse>(
                 queryable, query
             );
