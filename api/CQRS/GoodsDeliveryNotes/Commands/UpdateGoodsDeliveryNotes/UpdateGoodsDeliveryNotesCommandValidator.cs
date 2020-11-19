4f685701dfee150c8bcd500a7eeabba73c1ad61b
tortoise10h <tortoise10h@gmail.com>
2020-11-19 14:51:11 +0700

    [Feature] update goods delivery note

---
+++ api/CQRS/GoodsDeliveryNotes/Commands/UpdateGoodsDeliveryNotes/UpdateGoodsDeliveryNotesCommandValidator.cs
@@ -0,0 +1,37 @@
+﻿using api.Common.Enums;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.CQRS.GoodsDeliveryNotes.Commands.UpdateGoodsDeliveryNotes
+{
+    public class UpdateGoodsDeliveryNotesCommandValidator: AbstractValidator<UpdateGoodsDeliveryNotesCommand>
+    {
+        public UpdateGoodsDeliveryNotesCommandValidator()
+        {
+            RuleFor(x => x.Status)
+                .IsInEnum()
+                    .WithMessage("Trạng thái không hợp lệ");
+
+            When(x => x.Status == GoodsDeliveryNoteStatus.Cancelled, () =>
+            {
+                RuleFor(x => x.ExceptionReason)
+                    .NotEmpty()
+                        .WithMessage("Xin hãy nhập vào lý do");
+            });
+
+            When(x => x.Status == GoodsDeliveryNoteStatus.Pending ||
+                x.Status == GoodsDeliveryNoteStatus.Done || 
+                x.Status == GoodsDeliveryNoteStatus.New ||
+                x.Status == GoodsDeliveryNoteStatus.Approved,
+                () =>
+                {
+                    RuleFor(x => x.ExceptionReason)
+                        .Must(x => x == null || x == "")
+                            .WithMessage("Không được phép cập nhật lý do");
+                });
+        }
+    }
+}
--- api/IServices/IGoodsDeliveryNoteServices.cs
+++ api/IServices/IGoodsDeliveryNoteServices.cs
@@ -1,5 +1,7 @@
-﻿using api.Contracts.V1.Dtos;
+﻿using api.Common.Enums;
+using api.Contracts.V1.Dtos;
 using api.Entities;
+using api.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,5 +12,12 @@ namespace api.IServices
     public interface IGoodsDeliveryNoteServices
     {
         Task<List<ProductInGoodsDeliveryNote>> MakeSureProductsOfNewGoodsDeliveryNoteValid(List<ProductInGoodsDeliveryNote> productsInGoodsDeliveryNote, List<OrderDetail> orderDetails);
+
+        Task ValidateWhenUpdateStatus(GoodsDeliveryNote goodsDeliveryNote, GoodsDeliveryNoteStatus newStatus);
+
+        Task HandleBusinessWhenStatusIsChanged(
+            DataContext ctx,
+            GoodsDeliveryNote goodsDeliveryNote,
+            GoodsDeliveryNoteStatus newStatus);
     }
 }
