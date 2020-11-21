fd8599ba9d6f5883c74731ad5611c9de926f30e0
tortoise10h <tortoise10h@gmail.com>
2020-11-21 12:07:30 +0700

    [Feature] best selling products statistics

---
+++ api/Controllers/V1/StatisticsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Contracts.V1;
+using api.Contracts.V1.ResponseModels;
+using api.Contracts.V1.ResponseModels.Products;
+using api.Contracts.V1.ResponseModels.Statistics;
+using api.CQRS.Products.Commands.CreateProducts;
+using api.CQRS.Products.Commands.UpdateProducts;
+using api.CQRS.Products.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using src.CQRS.Products.Queries;
+using src.CQRS.PurchaseProposalForms.Queries;
+
+namespace api.Controllers.V1
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Admin,Boss")]
+    public class StatisticsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public StatisticsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet(ApiRoutes.Statistics.BestSellingProductsStatistic)]
+        public async Task<IActionResult> GetBestSellingProducts([FromQuery] BestSellingProductsStatisticQuery query)
+        {
+            var result = await _mediator.Send(query);
+
+            return result.Match<IActionResult>(
+                data => Ok(new Response<List<BestSellingProductsStatisticResponse>>(
+                    data
+                )),
+                exp =>
+                {
+                    throw exp;
+                }
+            );
+        }
+    }
+}
