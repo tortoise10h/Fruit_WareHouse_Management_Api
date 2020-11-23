d31b7cf857268cb48056aae77f9c3077f288794b
Huy Quyen <huyquyent@gmail.com>
2020-11-23 16:01:42 +0700

    [Fix] Remove Quantity Return when create Merchandise Return Detail

--- api/CQRS/MerchandiseReturnProposals/Commands/CreateMerchandiseReturnDetail/CreateMerchandiseReturnDetailCommand.cs
+++ api/CQRS/MerchandiseReturnProposals/Commands/CreateMerchandiseReturnDetail/CreateMerchandiseReturnDetailCommand.cs
@@ -6,7 +6,6 @@ namespace api.CQRS.MerchandiseReturnProposals.Commands.CreateMerchandiseReturnDe
     public class CreateMerchandiseReturnDetailCommand : IRequest<MerchandiseReturnDetailResponse>
     {
         public int Quantity { get; set; }
-        public int QuantityReturned { get; set; }
         public string Description { get; set; }
         public int ProductId { get; set; }
     }
--- api/CQRS/MerchandiseReturnProposals/Commands/UpdateMerchandiseReturnDetail/UpdateMerchandiseReturnDetailCommandValidator.cs
+++ api/CQRS/MerchandiseReturnProposals/Commands/UpdateMerchandiseReturnDetail/UpdateMerchandiseReturnDetailCommandValidator.cs
@@ -15,14 +15,6 @@ namespace api.CQRS.MerchandiseReturnProposals.Commands.UpdateMerchandiseReturnDe
             RuleFor(x => x.Quantity)
                 .GreaterThan(0)
                   .WithMessage("Số lượng trả lại của sản phẩm không được phép nhỏ hơn 0");
-
-            RuleFor(x => x.QuantityReturned)
-                   .NotNull()
-                       .WithMessage("Số lượng đã trả của sản phẩm không được để trống")
-                   .LessThanOrEqualTo(x => x.Quantity)
-                       .WithMessage("Số lượng đã trả của sản phẩm không được lớn hơn số lượng trả lại")
-                   .GreaterThan(0)
-                       .WithMessage("Số lượng đã trả của sản phẩm không được nhỏ hơn 0");
         }
     }
 }
