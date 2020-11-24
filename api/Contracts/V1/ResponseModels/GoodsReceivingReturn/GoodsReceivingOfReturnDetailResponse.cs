4f8bac78f72f3b60ba0aa90fdd01cff59ed978e6
tortoise10h <tortoise10h@gmail.com>
2020-11-24 09:28:41 +0700

    [Fix] fix goods receiving of return detail response

--- api/Contracts/V1/ResponseModels/GoodsReceivingReturn/GoodsReceivingOfReturnDetailResponse.cs
+++ api/Contracts/V1/ResponseModels/GoodsReceivingReturn/GoodsReceivingOfReturnDetailResponse.cs
@@ -1,7 +1,14 @@
+using api.Contracts.V1.ResponseModels.Products;
+
 namespace api.Contracts.V1.ResponseModels.GoodsReceivingReturn
 {
     public class GoodsReceivingOfReturnDetailResponse
     {
-
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int GoodsReceivingOfReturnId { get; set; }
+        public double Quantity { get; set; }
+        public string Description { get; set; }
+        public ProductResponse Product { get; set; }
     }
 }
